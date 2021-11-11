72f098178dbf3b5e93f1abf22bc0e82a37df688d
tstraeubig <tobias.straeubig@outlook.de>
2021-11-11 18:24:45 +0100

    submitted authors are now saved

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -13,4 +13,14 @@ describe "New author page", type: :feature do
     expect(page).to have_field('author[last_name]')
     expect(page).to have_field('author[homepage]')
   end
+
+  it "should save the authors" do
+    visit new_author_path
+    prev_count = Author.count
+    page.fill_in 'author[first_name]', with: 'Alan'
+    page.fill_in 'author[last_name]', with: 'Turing'
+    page.fill_in 'author[homepage]', with: 'http://wikipedia.org/Alan_Turing'
+    find('input[type="submit"]').click
+    expect(Author.count).to eq(prev_count + 1)
+  end
 end
