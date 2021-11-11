61780601f0fb98f3ced614b8a44001f38529f692
tstraeubig <tobias.straeubig@outlook.de>
2021-11-11 17:35:39 +0100

    added author model

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Author, type: :model do
+  it 'is constructed properly' do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author.first_name).to eq('Alan')
+    expect(author.last_name).to eq('Turing')
+    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+    expect(author.name).to eq("Alan Turing")
+  end
+end
