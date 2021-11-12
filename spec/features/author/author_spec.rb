0e6d882fa4d95bd9bb60cda5b3d7c73423596365
tstraeubig <tobias.straeubig@outlook.de>
2021-11-12 12:45:02 +0100

    added deleting of authors

--- spec/features/author/author_spec.rb
+++ spec/features/author/author_spec.rb
@@ -16,4 +16,17 @@ describe "Author index page", :type => :feature do
         visit authors_path
         expect(page).to have_link 'New', href: new_author_path
     end
+
+    context "with author named Alan Turing" do
+        alan = FactoryBot.create :author
+        it "should have a link to delete an Alan Turing" do
+            visit authors_path
+            prev_count = Author.count
+            expect(page).to have_selector(:css, "a[data-method='delete'][href='/authors/%d']" % [alan.id])
+            link = page.find(:css, "a[data-method='delete'][href='/authors/%d']" % [alan.id])
+            link.click
+            expect(Author.count).to eq(prev_count - 1)
+        end
+
+    end
 end
