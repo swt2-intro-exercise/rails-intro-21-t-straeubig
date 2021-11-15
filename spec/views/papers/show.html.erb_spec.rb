26fa60eea6325a0f356269dd7ebe196d3334f38c
tstraeubig <tobias.straeubig@outlook.de>
2021-11-15 16:10:35 +0100

    shows authors

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "papers/show", type: :view do
-  before(:each) do
-    @paper = assign(:paper, Paper.create!(
-      title: "Title",
-      venue: "Venue",
-      year: 2
-    ))
-  end
+  context "with paper from author" do
+    alan = FactoryBot.create :author
+    before(:each) do
+      @paper = assign(:paper, Paper.create!(
+        title: "Title",
+        venue: "Venue",
+        year: 2
+      ))
+      
+      @paper.authors.append(alan)
+    end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Venue/)
-    expect(rendered).to match(/2/)
+    it "renders attributes in <p>" do
+      render
+      expect(rendered).to match(/Title/)
+      expect(rendered).to match(/Venue/)
+      expect(rendered).to match(/2/)
+      expect(rendered).to match(/#{alan.name}/)
+    end
   end
 end
